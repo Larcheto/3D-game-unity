1bdd3533deff0905327de304457ff789e6f4cf21
Larcheto <lara.angelova@gmail.com>
2021-01-26 11:39:29 +0200

    Added walking animation controller

--- Game-Diploma/Assets/Scripts/PlayerController.cs
+++ Game-Diploma/Assets/Scripts/PlayerController.cs
@@ -14,12 +14,55 @@ public class PlayerController : MonoBehaviour
         public abstract void Execute();
     }
 
+    public class JumpFunction : Command
+    {
+        public override void Execute()
+        {
+            Jump();
+        }
+    }
+
+    public class TelekinesisFunction : Command
+    {
+        public override void Execute()
+        {
+            Telekinesis();
+        }
+    }
+
+
+    public static void Telekinesis()
+    {
+        
+    }
+
+    public static void Jump()
+    {
+        
+    }
+
+    public static void DoMove()
+    {
+        Command keySpace = new JumpFunction();
+        Command keyX = new TelekinesisFunction();
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            keySpace.Execute();
+        }
+
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            keyX.Execute();
+        }
+    }
     
-    //public CharacterController characterController;
+    
+    public CharacterController characterController;
     public float speed = 3;
     
 
-    //public Animator animator;
+    public Animator animator;
     
     // camera and rotation
     public Transform cameraHolder;
@@ -37,6 +80,11 @@ public class PlayerController : MonoBehaviour
         Rotate();
     }
 
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
 
     public void Rotate()
     {
@@ -57,13 +105,12 @@ public class PlayerController : MonoBehaviour
         float horizontalMove = Input.GetAxis("Horizontal");
         float verticalMove = Input.GetAxis("Vertical");
 
-        //if (characterController.isGrounded) verticalSpeed = 0;
-        //else 
-        verticalSpeed -= gravity * Time.deltaTime;
+        if (characterController.isGrounded) verticalSpeed = 0;
+        else verticalSpeed -= gravity * Time.deltaTime;
         Vector3 gravityMove = new Vector3(0, verticalSpeed, 0);
         
         Vector3 move = transform.forward * verticalMove + transform.right * horizontalMove;
-        //characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
+        characterController.Move(speed * Time.deltaTime * move + gravityMove * Time.deltaTime);
         
         //animator.SetBool("isWalking", verticalMove != 0 || horizontalMove != 0);
 
