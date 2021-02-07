68268077505ce8c0daae16cb328349e01dd1db18
Larcheto <lara.angelova@gmail.com>
2021-02-07 20:08:14 +0200

    Added panel on button click

--- Game-Diploma/Assets/Scripts/PanelOpener.cs
+++ Game-Diploma/Assets/Scripts/PanelOpener.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class PanelOpener : MonoBehaviour
 {
-    public GameObject panel;
+    public GameObject Panel;
+
     public void OpenPanel(){
-        
+        if(Panel != null){
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
     }
 }
