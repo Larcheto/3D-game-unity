42eb077a57ef9c1d7722730289c572dbce28bc9f
Larcheto <lara.angelova@gmail.com>
2021-02-13 22:22:56 +0200

    Added link opener script to open quiz

---
+++ Game-Diploma/Assets/Scripts/QuizOpener.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizOpener : MonoBehaviour
+{
+    public void OpenQuiz(){
+         Application.OpenURL("http://unity3d.com/");
+    }
+}
