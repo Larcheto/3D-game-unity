c4f7c83d03dc84c7cf886ba16a8f86735a4f78dd
Larcheto <lara.angelova@gmail.com>
2021-04-24 22:38:57 +0300

    Added questions and style to the quiz

--- Svelte-App/svelte-quiz/src/App.svelte
+++ Svelte-App/svelte-quiz/src/App.svelte
@@ -1,33 +1,101 @@
 <script>
-	let questions = [];
+	let questions = [
+		{
+			"question": "Кога е построена къщата музей на Иван Вазов в София?",
+			"options": [
+				"1800г.",
+				"1895г.",
+				"1880г."
+			],
+			"correctIndex": 1
+		},
+		{
+			"question": "В коя стая е разположена личната библиотека на писателя?",
+			"options": [
+				"Гостната стая",
+				"Личния кабинет",
+				"Хола"
+			],
+			"correctIndex": 1
+		},
+		{
+			"question": "През коя година е роден Иван Вазов?",
+			"options": [
+				"1850",
+				"1921",
+				"1860",
+				"1800"
+			],
+			"correctIndex": 0
+		}
+	];
 	let answers = new Array(questions.length).fill(null);
 	let questionPointer = -1;
 
 	function getScore(){
-			let score = answers.reduce((acc,val,index)=>{
-				if(questions[index].correctIndex == val){
-					return acc+1;
-				}
-				return acc;
-			},0);
-			return (score/questions.length * 100)+"%";
-		}
+		let score = answers.reduce((acc,val,index)=>{
+			if(questions[index].correctIndex == val){
+				return acc+1;
+			}
+			return acc;
+		},0);
+		return (score/questions.length * 100).toFixed(0)+"%";
+	}
 </script>
 
 <style>
-	
+	.main{
+		height: 10em;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin-top:10%;
+	}
+	.main .start-button button,
+	.main .question-component .options button{
+		padding: 1rem;
+		background-color: rgb(38, 112, 173);
+		font-size: large;
+		color:white;
+		border-radius: 0.5rem;
+		height: 70px;
+		width: 140px;
+
+	}
+	.main .question-component .options button.selected{
+		background-color: rgb(15, 60, 97);
+	}
+
+	.main .question-component,
+	.main .score-component{
+		padding: 5%;
+		height: 15rem;
+		width: 40rem;
+		border-radius: 2%;
+		box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+		text-align: center;
+	}
+	.main .question-component .options{
+		margin: 5%;
+		display: flex;
+		justify-content: center;
+	}
+	.main .score-component .fb-share-button{
+		margin: 7%
+	}
+
 </style>
 
-<div class="app">
+<div class="main">
 	{#if questionPointer==-1}
-		<div class="start-screen">
+		<div class="start-button">
 			<button on:click={()=>{questionPointer=0}}>
 				Start Quiz
 			</button>	
 		</div>
 	{:else if !(questionPointer > answers.length-1)}
-		<div class="quiz-screen">
-			<div class="main">
+		<div class="question-component">
+			<div class="title">
 				<h2>
 					{questions[questionPointer].question}
 				</h2>
@@ -56,13 +124,13 @@
 				src="https://connect.facebook.net/bg_BG/sdk.js#xfbml=1&version=v10.0"
 				nonce="XFt0SvXB">
 		</script>
-		<div class="score-screen">
+		<div class="score-component">
 			<h1>
 				Your score: {getScore()}
 			</h1>
 			<div class="fb-share-button"
 				 data-href="http://localhost:5000/" 
-				 data-layout="button" data-size="small">
+				 data-layout="button" data-size="large">
 				 <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Flocalhost%3A5000%2F&amp;src=sdkpreparse"
 				 class="fb-xfbml-parse-ignore">Share with friends!</a>
 			</div>
